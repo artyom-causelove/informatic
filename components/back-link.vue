84805eea8ea46d74db9d243290df4216e10ac988
Artem Bondareko <artemzi1000@gmail.com>
2025-02-10 22:21:29 +0700

    refactor(components): migrate back-link to Composition API with TypeScript

--- components/back-link.vue
+++ components/back-link.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const router = useRouter();
+</script>
+
 <template>
-  <NuxtLink class="link" @click="$router.back()">
-    <img class="image" src="/paper-back.png" width="195" height="110" />
+  <NuxtLink class="link" @click="router.back()">
+
+    <img class="image" src="/paper-back.png" width="195" height="110">
   </NuxtLink>
 </template>
