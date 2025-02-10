2c3fc5b15c6ae1330b1b89dbb692b1d4fa040517
Artem Bondareko <artemzi1000@gmail.com>
2025-02-10 22:21:40 +0700

    init work page

---
+++ pages/works/[id].vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const { id } = useRoute().params;
+</script>
+
+<template>
+  <NuxtLayout name="default">
+    <template #header>
+      <div>header {{ id }}</div>
+    </template>
+    <template #main>
+      <div>main</div>
+    </template>
+  </NuxtLayout>
+</template>
+
+<style></style>
