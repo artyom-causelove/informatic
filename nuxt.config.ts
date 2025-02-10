da997bd1c187871682cd5a267054b711d3d0069a
Artem Bondareko <artemzi1000@gmail.com>
2025-02-10 22:13:49 +0700

    chore: add ESLint for code quality and linting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: [],
+  modules: ['@nuxt/eslint'],
   css: [
     '@/assets/css/main.css'
   ],
